c23970eea3d67dcf2df75c92fd91fd57b1f78733
Gunay Karli <gunaykarli@gmail.com>
2019-01-15 11:52:25 +0100

    Services, LawText tables and their many to many relationships with VodafoneTariff have been created...

--- app/Tariff.php
+++ app/Tariff.php
@@ -25,6 +25,13 @@ class Tariff extends Model
     }
 
     public function dealers(){
+        //** "ontop" is pivot table for many to many relationship between dealer and tariff
+        // Generally it is defined as dealer_tariff but in this case we have special name "ontop" */
         return $this->belongsToMany(Dealer::class, 'ontop');
     }
+
+    public function vodafoneTariff(){
+        return $this->hasOne(VodafoneTariff::class);
+    }
+
 }
