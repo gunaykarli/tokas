ba081ca7b208101ef438c1d342182eb5e9871b7e
Gunay Karli <gunaykarli@gmail.com>
2019-04-12 12:28:44 +0200

    Tariffs can be fetched with filter using ONLY "TariffController@fetchTariffWithFilter"

--- public/js/tariffListWithFilter.js
+++ public/js/tariffListWithFilter.js
@@ -10,13 +10,14 @@ $(document).ready(function(){
         alert('radioInLineProviders: ' + providerID);
 
         $.ajax({
-            url:"/tariff/index/tariff-groups",
+            url:"/tariff/index/tariffs-with-filter",
             method:"Post",
             data:{providerID:providerID, tariffGroupID:tariffGroupID, maxBasePrice:maxBasePrice, _token:_token},
 
             success:function(responses)
             {
                 alert('radioInLineProviders in success: ');
+                // list the tariff groups
                 $("#radioInLineGroups").empty();
 
                 $("#radioInLineGroups").append( "<label class='m-radio'>");
@@ -29,6 +30,7 @@ $(document).ready(function(){
                     $("#radioInLineGroups").append( "<span></span>" + "</label>");
                 });
 
+                // list the tariffs consisted of "out"
                 $("#tableBody").empty();
                 $("#tableBody").html(responses["out"]);
             }
@@ -45,13 +47,16 @@ $(document).ready(function(){
         alert('tariffGroup: ' + tariffGroupID);
 
         $.ajax({
-            url:"/tariff/index/groups-tariff-list",
+            //url:"/tariff/index/groups-tariff-list",
+            url:"/tariff/index/tariffs-with-filter",
             method:"Post",
             data:{providerID:providerID, tariffGroupID:tariffGroupID, maxBasePrice:maxBasePrice, _token:_token},
-            success:function(data) {
+            success:function(responses) {
                 alert('radioInLineGroups-In success: ');
+                // No need to list tariff groups since they are already there...
+                // Just list the tariffs consisted of "out"
                 $("#tableBody").empty();
-                $("#tableBody").html(data);
+                $("#tableBody").html(responses['out']);
             }
         })
     });
@@ -73,19 +78,8 @@ $(document).ready(function(){
             {
                 alert('filterPortlet: in success ');
 
-                $("#radioInLineGroups").empty();
-
-                $("#radioInLineGroups").append( "<label class='m-radio'>");
-                $("#radioInLineGroups").append( "<input type='radio' name='tariffGroup' id='tariffGroup' class='m-radio' checked value=" + 0 + ">" + "All");
-                $("#radioInLineGroups").append( "<span></span>" + "</label>");
-
-                if(!(responses["tariffGroups"] == ""))
-                    $.each(responses["tariffGroups"], function(key, value) {
-                        $("#radioInLineGroups").append( "<label class='m-radio'>");
-                        $("#radioInLineGroups").append( "<input type='radio' name='tariffGroup' id='tariffGroup' class='m-radio' value=" + value.id + ">" + value.name );
-                        $("#radioInLineGroups").append( "<span></span>" + "</label>");
-                    });
-
+                // No need to list tariff groups since they are already there...
+                // Just list the tariffs consisted of "out"
                 $("#tableBody").empty();
                 $("#tableBody").html(responses["out"]);
             }
@@ -101,15 +95,20 @@ $(document).ready(function(){
 
         alert('tariffGroup: ' + tariffGroupID);
 
-        $('#maxBasePrice').val('').empty();
+
         $.ajax({
-            url:"/tariff/index/groups-tariff-list",
+            //url:"/tariff/index/groups-tariff-list",
+            url:"/tariff/index/tariffs-with-filter",
             method:"Post",
             data:{providerID:providerID, tariffGroupID:tariffGroupID, _token:_token},
-            success:function(data) {
+            success:function(responses) {
+                // Empty the filter parameters...
+                $('#maxBasePrice').val('').empty();
                 alert('radioInLineGroups-In success: ');
+                // No need to list tariff groups since they are already there...
+                // Just list the tariffs consisted of "out"
                 $("#tableBody").empty();
-                $("#tableBody").html(data);
+                $("#tableBody").html(responses["out"]);
             }
         })
     });
